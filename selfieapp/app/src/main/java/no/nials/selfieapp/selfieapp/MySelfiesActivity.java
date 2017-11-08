8c31defc5f6fe6d72a7f5935a8a21810a7be0954
dawyjones <he-vt@hotmail.com>
2017-11-08 13:18:00 +0100

    MySelfiesActivity (IN PROGRESS)
    Kan laste opp lokale bilder til listview. 8 test bilder lokalt lagt til.

---
+++ selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/MySelfiesActivity.java
@@ -0,0 +1,68 @@
+package no.nials.selfieapp.selfieapp;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MySelfiesActivity extends Activity{
+
+    ListView list;
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+
+    Integer[] imgid={
+            R.drawable.pic1,
+            R.drawable.pic2,
+         R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.pic7,
+            R.drawable.pic8,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myselfies);
+
+        MySelfiesListAdapter adapter=new MySelfiesListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+}
