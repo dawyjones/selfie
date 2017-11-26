2b1fd7a1129c62cdeee485041cbc47b19969a5dc
kristofferlie <kristoffer.g.lie@gmail.com>
2017-11-26 19:34:15 +0100

    asynctreg

---
+++ selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/AsyncT.java
@@ -0,0 +1,104 @@
+package no.nials.selfieapp.selfieapp;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class AsyncT extends AsyncTask<Void, Void, String> {
+
+    String password;
+    String email;
+
+    public AsyncT(String email, String password){
+
+        this.password = password;
+        this.email = email;
+    }
+
+
+
+    @Override
+    protected String doInBackground(Void... params) {
+        try {
+            String address = "http://nials.no:8080/selfie/api/user/newUser";
+            JSONObject json = new JSONObject();
+            json.put("email", email);
+            json.put("password", password);
+            String requestBody = json.toString();
+            URL url = new URL(address);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
+            writer.write(requestBody);
+            writer.flush();
+            writer.close();
+            outputStream.close();
+
+            InputStream inputStream;
+            // get stream
+            if (urlConnection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                inputStream = urlConnection.getInputStream();
+            } else {
+                inputStream = urlConnection.getErrorStream();
+            }
+            // parse stream
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String temp, response = "";
+            while ((temp = bufferedReader.readLine()) != null) {
+                response += temp;
+            }
+
+        } catch (IOException | JSONException e) {
+            return e.toString();
+        }
+        return null;
+
+/*
+        try {
+            URL url = new URL("http://192.168.1.13:8080/messenger/webapi/messages"); //Enter URL here
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
+            httpURLConnection.setRequestProperty("Content-Type", "application/json"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
+            httpURLConnection.connect();
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("author", "0");
+            jsonObject.put("message", picture);;
+
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+            wr.writeBytes(jsonObject.toString());
+            wr.flush();
+            wr.close();
+
+            String jsonString = jsonObject.toString();
+            Log.d("SONDRE",jsonString);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null
+        */
+    }
+
+}
