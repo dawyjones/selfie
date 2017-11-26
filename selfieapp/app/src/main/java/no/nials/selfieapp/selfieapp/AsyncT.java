920d538d6791aa966d09d98f37a8bed24e4b4d9b
kristofferlie <kristoffer.g.lie@gmail.com>
2017-11-26 21:28:15 +0100

    got it

--- selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/AsyncT.java
+++ selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/AsyncT.java
@@ -2,6 +2,7 @@ package no.nials.selfieapp.selfieapp;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,33 +17,51 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-class AsyncT extends AsyncTask<Void, Void, String> {
+class AsyncT extends AsyncTask<String, String, String> {
 
-    String password;
-    String email;
+    String phone, email, password, name, birthday, gender;
 
-    public AsyncT(String email, String password){
+    public AsyncT(String email, String password, String phone, String name, String birthday, String gender){
 
         this.password = password;
         this.email = email;
+        this.name= name;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.gender = gender;
     }
 
 
 
     @Override
-    protected String doInBackground(Void... params) {
+    protected String doInBackground(String... strings) {
         try {
-            String address = "http://nials.no:8080/selfie/api/user/newUser";
+            String address = "http://nials.no:8080/selfie/api/user/newUser?name=" + name + "&birthday=" + birthday + "&email=" +email +"&password=" + password +"&phone=" +phone + "&gender=" + gender;
+            System.out.println(address);
             JSONObject json = new JSONObject();
-            json.put("email", email);
-            json.put("password", password);
+            json.put("name", "kys");
+            json.put("birthday", "1990-01-01");
+            json.put("email", "ken@ken.com");
+            json.put("password", "123");
+            json.put("phone", "12345678");
+            json.put("gender", "m");
+
+
+          //  JSONArray array = new JSONArray();
+
+
+           // array.put(json);
             String requestBody = json.toString();
+            System.out.println("test:  " + requestBody);
             URL url = new URL(address);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setDoOutput(true);
             urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(5000);
             OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
+            //outputStream.write(requestBody)
             writer.write(requestBody);
             writer.flush();
             writer.close();
--- selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/MainActivity.java
+++ selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editTextPhone, editTextEmail, editTextPassword, editTextBirthday, editTextName;
     RadioGroup radioGroupGender;
+    String phone,  name, birthday,  gender;
 
 
     @Override
@@ -31,26 +33,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //if the user is already logged in we will directly start the profile activity
-        if (SharedPrefManager.getInstance(this).isLoggedIn()) {
-            finish();
-            startActivity(new Intent(this, ProfileActivity.class));
-            return;
-        }
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextPhone = (EditText) findViewById(R.id.editTextPhone);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextBirthday = (EditText) findViewById(R.id.editTextBirthday);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         radioGroupGender = (RadioGroup) findViewById(R.id.radioGender);
+       Button btnregister = (Button) findViewById(R.id.buttonRegister);
 
 
-        findViewById(R.id.buttonRegister).setOnClickListener(new View.OnClickListener() {
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //if user pressed on button register
                 //here we will register the user to server
-                registerUser();
+                final String email = editTextEmail.getText().toString().trim();
+                final String password = editTextPassword.getText().toString().trim();
+                final String phone = editTextPhone.getText().toString().trim();
+                final String name = editTextName.getText().toString().trim();
+                final String birthday = editTextBirthday.getText().toString().trim();
+                final String gender = "M";
+                AsyncT asyncT = new AsyncT(email, password,  phone,  name, birthday,  gender);
+                System.out.println("email : " + email + "   password:  " + password);
+                asyncT.execute();
             }
         });
 
@@ -65,131 +71,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-    private void registerUser() {
-        final String name = editTextName.getText().toString().trim();
-        final String phone1 = editTextPhone.getText().toString().trim();
-        final int phone = Integer.parseInt(phone1);
-        final String email = editTextEmail.getText().toString().trim();
-        final String birthday = editTextBirthday.getText().toString().trim();
-        final String password = editTextPassword.getText().toString().trim();
-        final String gender = ((RadioButton) findViewById(radioGroupGender.getCheckedRadioButtonId())).getText().toString();
-
-        //first we will do the validations
-
-
-        if (TextUtils.isEmpty(phone1)) {
-            editTextEmail.setError("Please enter your phonenumber");
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (TextUtils.isEmpty(birthday)) {
-            editTextEmail.setError("Please enter your birthday");
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (TextUtils.isEmpty(email)) {
-            editTextEmail.setError("Please enter your email");
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextEmail.setError("Enter a valid email");
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (TextUtils.isEmpty(password)) {
-            editTextPassword.setError("Enter a password");
-            editTextPassword.requestFocus();
-            return;
-        }
-
-        //if it passes all the validations
-
-        class RegisterUser extends AsyncTask<Void, Void, String> {
-
-            private ProgressBar progressBar;
-
-            @Override
-            protected String doInBackground(Void... voids) {
-                //creating request handler object
-                RequestHandler requestHandler = new RequestHandler();
-                HashMap<String, String> params = new HashMap<>();
-
-
-
-                String param1 = name;
-                String param2 = phone1;
-                String param3 = email;
-                String param4 = password;
-                String param5 = birthday;
-                String param6 = gender;
-
-                //returing the response
-                return requestHandler.sendPostRequest("http://nials.no:8080/api/api/user/newUser",params);
-            }
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                //displaying the progress bar while user registers on the server
-                progressBar = (ProgressBar) findViewById(R.id.progressBar);
-                progressBar.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                //hiding the progressbar after completion
-                progressBar.setVisibility(View.GONE);
-                //String url = "http://nials.no:8080/api/api/user/newUser";
-                try {
-                    //converting response to json object
-                    //
-                    // JSONObject obj = new JSONObject(s);
-
-                    JSONObject obj = new JSONObject(s);
-
-
-                    //if no error in response
-                    if (!obj.getBoolean("error")) {
-                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-
-                        //getting the user from the response
-                        JSONObject userJson = obj.getJSONObject("user");
-
-                        //creating a new user object
-                        User user = new User(
-                               // userJson.getInt("id"),
-                                userJson.getString("name"),
-                                userJson.getString("email"),
-                                userJson.getString("birthday"),
-                                userJson.getString("phone"),
-                                userJson.getString("gender")
-                        );
-
-                        //storing the user in shared preferences
-                        SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
-
-                        //starting the profile activity
-                        finish();
-                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Some error occurred", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        //executing the async task
-        RegisterUser ru = new RegisterUser();
-        ru.execute();
-    }
-
 }
+
+
