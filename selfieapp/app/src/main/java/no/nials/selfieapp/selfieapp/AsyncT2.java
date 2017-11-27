86eab6319217508c1d29eee769294e4960f282d9
kristofferlie <kristoffer.g.lie@gmail.com>
2017-11-27 13:58:43 +0100

    Registrering fungerer

---
+++ selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/AsyncT2.java
@@ -0,0 +1,96 @@
+package no.nials.selfieapp.selfieapp;
+
+/**
+ * Created by Kami on 21.11.2017.
+ */
+
+
+        import android.os.AsyncTask;
+
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+        import java.io.BufferedOutputStream;
+        import java.io.BufferedReader;
+        import java.io.BufferedWriter;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.io.OutputStream;
+        import java.io.OutputStreamWriter;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
+
+class AsyncT2 extends AsyncTask<String, String, String> {
+
+    String phone, email, password, name, birthday, gender;
+
+    public AsyncT2(String email, String password, String phone, String name, String birthday, String gender){
+
+        this.password = password;
+        this.email = email;
+        this.name= name;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.gender = gender;
+    }
+
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            String address = "http://nials.no:8080/selfie/api/user/newUser?name=" + name + "&birthday=" + birthday + "&email=" +email +"&password=" + password +"&phone=" +phone + "&gender=" + gender;
+            System.out.println(address);
+            JSONObject json = new JSONObject();
+            json.put("name", "name");
+            json.put("birthday", "1990-01-01");
+            json.put("email", "ken@ken.com");
+            json.put("password", "123");
+            json.put("phone", "12345678");
+            json.put("gender", "m");
+
+
+            //  JSONArray array = new JSONArray();
+
+
+            // array.put(json);
+            String requestBody = json.toString();
+            System.out.println("test:  " + requestBody);
+            URL url = new URL(address);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(5000);
+            OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
+            //outputStream.write(requestBody)
+            writer.write(requestBody);
+            writer.flush();
+            writer.close();
+            outputStream.close();
+
+            InputStream inputStream;
+            // get stream
+            if (urlConnection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                inputStream = urlConnection.getInputStream();
+            } else {
+                inputStream = urlConnection.getErrorStream();
+            }
+            // parse stream
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String temp, response = "";
+            while ((temp = bufferedReader.readLine()) != null) {
+                response += temp;
+            }
+
+        } catch (IOException | JSONException e) {
+            return e.toString();
+        }
+        return null;
+
+    }
+
+}
--- selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/Login.java
+++ selfieapp/app/src/main/java/no/nials/selfieapp/selfieapp/Login.java
@@ -1,266 +1,74 @@
 package no.nials.selfieapp.selfieapp;
 
-/**
- * Created by Kristoffer on 11/22/2017.
- */
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Base64;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
+public class Login extends AsyncTask<Void,Void,Void> {
+    String data ="";
+    String dataParsed = "";
+    String singleParsed ="";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("http://nials.no:8080/selfie/api/user");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONArray JA = new JSONArray(data);
+            for(int i = JA.length() - 1 ;i <JA.length(); i++){
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed =
+                        JO.get("email") + "\n";
+
+                dataParsed = dataParsed + singleParsed +"\n" ;
 
 
-        import android.app.ProgressDialog;
-        import android.content.Intent;
-        import android.os.AsyncTask;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+            }
 
-        import android.util.JsonReader;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.Toast;
-        import org.json.JSONArray;
-        import org.json.JSONException;
-        import org.json.JSONObject;
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.InputStreamReader;
-        import java.net.HttpURLConnection;
-        import java.net.MalformedURLException;
-        import java.net.URL;
-        import java.text.ParseException;
-        import java.text.SimpleDateFormat;
-        import java.util.ArrayList;
-        import java.util.List;
-        import java.util.Date;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-public class Login extends AppCompatActivity {
-    EditText  editTextEmail, editTextPassword;
-    // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
-    public static final int CONNECTION_TIMEOUT = 10000;
-    public static final int READ_TIMEOUT = 15000;
-
-
-    List<User> Userlist = new ArrayList<>();
-
+        return null;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        final EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        final EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
 
-        final Button bLogin = (Button) findViewById(R.id.buttonLogin);
+        //StreamActivity.data.setText(dataParsed);
 
 
-        bLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        byte[] imageBytes = Base64.decode(dataParsed, Base64.DEFAULT);
+        Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
 
-                new AsyncFetch().execute();
-
-            }
-
-        });   //Make call to AsyncTask'
-
-        findViewById(R.id.textViewRegister).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //open register screen
-                startActivity(new Intent(Login.this, MainActivity.class));
-            }
-        });
+        StreamActivity.imageView.setImageBitmap(decodedImage);
 
 
     }
-
-
-
-    protected class AsyncFetch extends AsyncTask<String, String, String> {
-        ProgressDialog pdLoading = new ProgressDialog(Login.this);
-        HttpURLConnection conn;
-        URL url = null;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            //this method will be running on UI thread
-            pdLoading.setMessage("\tLoading...");
-            pdLoading.setCancelable(false);
-            pdLoading.show();
-
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-
-                // Enter URL address where your json file resides
-                // Even you can make call to php file which returns json data
-                url = new URL("http://nials.no:8080/selfie/api/user");
-
-            } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return e.toString();
-            }
-            try {
-
-                // Setup HttpURLConnection class to send and receive data from php and mysql
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(READ_TIMEOUT);
-                conn.setConnectTimeout(CONNECTION_TIMEOUT);
-                conn.setRequestMethod("GET");
-
-                // setDoOutput to true as we recieve data from json file
-                //conn.setDoOutput(true);
-
-                //con = (HttpURLConnection)urls[0].openConnection();
-                JsonReader jr = new JsonReader(new InputStreamReader(conn.getInputStream()));
-                jr.beginArray();
-                System.out.println("HEI" + jr);
-
-                InputStream input = conn.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                StringBuilder result = new StringBuilder();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-
-                // Pass data to onPostExecute method
-
-
-
-                while (jr.hasNext()) {
-                    System.out.println("JR has next element");
-                    String email = null;
-                    int id = 0;
-                    String gender = null;
-                    String birthday = null;
-                    String name = null;
-                    String password = null;
-                    int phone = 123;
-
-
-
-                    jr.beginObject();
-                    while (jr.hasNext()) {
-                        switch (jr.nextName()) {
-                            case "email":
-                                //System.out.println("HEI ID: " + jr.nextString());
-                                email = jr.nextString();
-                                result.append(email);
-                                break;
-                            /** case "size":
-                             //size = jr.nextLong();
-                             //break;
-                             **/
-                            case "password":
-                                // try {
-                                //version = format.parse(jr.nextString());
-                                password = jr.nextString();
-                                // } catch (ParseException e) {
-                                //   Log.e("LoadThumb", "Failed to parse date", e);
-                                // }
-                                break;
-                            default:
-                                jr.skipValue();
-
-
-                                // Pass data to onPostExecute method
-                                System.out.println("Result to string: " + result.toString());
-
-
-                        }
-                    }
-                    jr.endObject();
-                    String emailtext = "teit"; String passwordtext = "teit";
-                    Userlist.add(new User(id, email, password, phone, name, birthday, gender));
-                    if((emailtext == email) && (passwordtext == password)){
-                        startActivity(new Intent(Login.this, MenuActivity.class));
-
-                    }
-                    startActivity(new Intent(Login.this, MenuActivity.class));
-                }
-                System.out.println("RESULT STRING: " +result.toString());
-                return (result.toString());
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return e.toString();
-            }
-
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            //this method will be running on UI thread
-            System.out.println("HEI to string :" + result.toString());
-            //System.out.println("HEI :" + result);
-            pdLoading.dismiss();
-            List<User> data = new ArrayList<>();
-
-            pdLoading.dismiss();
-            /**for (Photo p : PhotoList) {
-
-             }**/
-            // mRVFishPrice = (RecyclerView) findViewById(R.id.fishPriceList);
-            // mAdapter = new AdapterFish(MainActivity.this, Userlist);
-            //  mRVFishPrice.setAdapter(mAdapter);
-            // mRVFishPrice.setLayoutManager(new LinearLayoutManager(Login.this));
-            /**
-             try {
-
-             JSONArray jArray = new JSONArray(result);
-
-             // Extract data from json and store into ArrayList as class objects
-             for (int i = 0; i < jArray.length(); i++) {
-             System.out.println("HEI INNENFOR FORLOKKE");
-             JSONObject json_data = jArray.getJSONObject(i);
-
-             JSONObject obj = new JSONObject(response);
-             String dateStr = obj.getString("birthdate");
-             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-             Date birthDate = sdf.parse(dateStr);
-             //then
-             user.setBirthdate(birthDate);
-
-
-             String url = json_data.getString("id");
-             String date = json_data.getString("version");
-             System.out.println("URL: " + url + "DATOOO: " + date);
-
-
-             Photo fishData = new Photo(url, date);
-             fishData.catName= json_data.getString("cat_name");
-             fishData.sizeName= json_data.getString("size_name");
-             fishData.price= json_data.getInt("price");
-             data.add(fishData);
-             }
-
-
-             // Setup and Handover data to recyclerview
-             mRVFishPrice = (RecyclerView) findViewById(R.id.fishPriceList);
-             mAdapter = new AdapterFish(MainActivity.this, data);
-             mRVFishPrice.setAdapter(mAdapter);
-             mRVFishPrice.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-             } catch (JSONException e) {
-             Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-             }
-             **/
-            // Setup and Handover data to recyclerview
-
-
-        }
-
-
-    }}
+}
